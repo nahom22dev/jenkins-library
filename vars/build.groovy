b4d59b2b584a81e94bbdbda9924d4685fec6a8d5
nahom22dev <110931115+nahom22dev@users.noreply.github.com>
2024-04-02 14:48:45 +0000

    Add createGithubRelease in build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,6 +1,8 @@
-
-
 def call(Map pipelineParams = [:]) {
+
+    def APP_NAME = "app-name"
+    def BRANCH_NAME = "main"
+
     pipeline {
         agent any
 
@@ -8,8 +10,20 @@ def call(Map pipelineParams = [:]) {
             stage('Hello') {
                 steps {
                     echo 'Hello World'
+                    createGithubRelease(repo: "owner/repo")
                 }
             }
+            // stage('Create Release') {
+            //     steps {
+            //         script {
+            //             catchError {
+            //                 sh """
+            //                     echo "curl github-api/create-release"
+            //                 """
+            //             }
+            //         }
+            //     }
+            // }
         }
     }
 }
---
+++ vars/createGithubRelease.groovy
@@ -0,0 +1,3 @@
+def call(String repo) {
+    echo "$repo"
+}
