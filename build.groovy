66d7a07bcecda92cfed15dec9e93f61b86ee4dd0
nahom22dev <110931115+nahom22dev@users.noreply.github.com>
2024-04-02 14:13:01 +0000

    Create build.groovy

---
+++ build.groovy
@@ -0,0 +1,15 @@
+
+
+def call(Map pipelineParams = [:]) {
+    pipeline {
+        agent any
+
+        stages {
+            stage('Hello') {
+                steps {
+                    echo 'Hello World'
+                }
+            }
+        }
+    }
+}
